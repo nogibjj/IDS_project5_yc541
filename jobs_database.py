e1a81df41066429165eeb2a8bd62b03e767da732
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-10-02 02:10:46 +0000

    update test

--- jobs_database.py
+++ jobs_database.py
@@ -1,13 +1,16 @@
 import sqlite3
 import os
 
+
 def connectDB(database_name):
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
     return conn, cursor
 
+
 def createDB(cursor):
-    cursor.execute('''
+    cursor.execute(
+        """
     CREATE TABLE IF NOT EXISTS jobs (
         job_id INTEGER PRIMARY KEY,
         company_name TEXT,
@@ -16,31 +19,45 @@ def createDB(cursor):
         job_type TEXT,
         position_count INTEGER
     )
-    ''')
+    """
+    )
 
-def insertDB(cursor, job_id, company_name, job_name, posted_date, job_type, position_count):
-    cursor.execute("INSERT INTO jobs (job_id, company_name, job_name, posted_date, job_type, position_count) VALUES (?, ?, ?, ?, ?, ?)",
-                   (job_id, company_name, job_name, posted_date, job_type, position_count))
+
+def insertDB(
+    cursor, job_id, company_name, job_name, posted_date, job_type, position_count
+):
+    cursor.execute(
+        "INSERT INTO jobs (job_id, company_name, job_name, posted_date, job_type, position_count) VALUES (?, ?, ?, ?, ?, ?)",
+        (job_id, company_name, job_name, posted_date, job_type, position_count),
+    )
+
 
 def readDB(cursor):
     cursor.execute("SELECT * FROM jobs")
     return cursor.fetchall()
 
+
 def updateDB(cursor, job_id, new_posted_date):
-    cursor.execute("UPDATE jobs SET posted_date = ? WHERE job_id = ?", (new_posted_date, job_id))
+    cursor.execute(
+        "UPDATE jobs SET posted_date = ? WHERE job_id = ?", (new_posted_date, job_id)
+    )
+
 
 def deleteDB(cursor, job_id):
     cursor.execute("DELETE FROM jobs WHERE job_id = ?", (job_id,))
+
 
 def query1(cursor):
     cursor.execute("SELECT COUNT(*) FROM jobs")
     count = cursor.fetchone()[0]
     return count
 
+
 def query2(cursor):
     cursor.execute("SELECT * FROM jobs WHERE posted_date < '2022-01-01'")
     older_jobs = cursor.fetchall()
     return older_jobs
+
 
 def main():
     database_name = "jobsDB.db"
@@ -51,14 +68,44 @@ def main():
 
     # C
     createDB(cursor)
-    insertDB(cursor, 55176, "Amazon", "Backend Developer", "2023-09-28", "experienced", 500)
-    insertDB(cursor, 55176, "Amazon", "Associate Software Develope", "2023-09-27", "new grads", 400)
-    insertDB(cursor, 87356, "LinkedIn", "Fullstack Developer", "2023-09-27", "new grads", 300)
+    insertDB(
+        cursor, 55176, "Amazon", "Backend Developer", "2023-09-28", "experienced", 500
+    )
+    insertDB(
+        cursor,
+        55176,
+        "Amazon",
+        "Associate Software Develope",
+        "2023-09-27",
+        "new grads",
+        400,
+    )
+    insertDB(
+        cursor, 87356, "LinkedIn", "Fullstack Developer", "2023-09-27", "new grads", 300
+    )
     insertDB(cursor, 99345, "Google", "Technolog Analyst", "2023-09-26", "intern", 200)
-    insertDB(cursor, 70001, "Apple", "Software Engineer", "2023-09-25", "new grads", 100)
-    insertDB(cursor, 80002, "Oracle", "Associate Software Developer", "2023-09-24", "experienced", 250)
-    insertDB(cursor, 90102, "Veeva System", "Associate Software Developer", "2023-09-24", "new grads", 350)
-    
+    insertDB(
+        cursor, 70001, "Apple", "Software Engineer", "2023-09-25", "new grads", 100
+    )
+    insertDB(
+        cursor,
+        80002,
+        "Oracle",
+        "Associate Software Developer",
+        "2023-09-24",
+        "experienced",
+        250,
+    )
+    insertDB(
+        cursor,
+        90102,
+        "Veeva System",
+        "Associate Software Developer",
+        "2023-09-24",
+        "new grads",
+        350,
+    )
+
     # R
     print(readDB(cursor))
 
@@ -76,6 +123,7 @@ def main():
 
     conn.commit()
     conn.close()
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,62 +1,108 @@
-from jobs_database import connectDB, createDB, insertDB, readDB, updateDB, deleteDB, query1, query2
+from jobs_database import (
+    connectDB,
+    createDB,
+    insertDB,
+    readDB,
+    updateDB,
+    deleteDB,
+    query1,
+    query2,
+)
 import os
 
 DATABASE_NAME = "jobs_database.db"
 
-def setup_function():
-    if os.path.exists(DATABASE_NAME):
-        os.remove(DATABASE_NAME)
+
 
 def test_create_and_insert():
     conn, cursor = connectDB(DATABASE_NAME)
     createDB(cursor)
-    insertDB(cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500)
-    
+    insertDB(
+        cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500
+    )
+
     cursor.execute("SELECT * FROM jobs WHERE job_id=?", (55176,))
     result = cursor.fetchone()
-    assert result == (55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500)
+    assert result == (
+        55176,
+        "Amazon",
+        "Software Engineer",
+        "2022-09-30",
+        "New Grads",
+        500,
+    )
+
 
 def test_read():
     conn, cursor = connectDB(DATABASE_NAME)
     createDB(cursor)
-    insertDB(cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500)
-    insertDB(cursor, 87356, "Google", "Backend Developer", "2022-09-20", "Experienced", 300)
-    
+    insertDB(
+        cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500
+    )
+    insertDB(
+        cursor, 87356, "Google", "Backend Developer", "2022-09-20", "Experienced", 300
+    )
+
     info = readDB(cursor)
     assert len(info) == 2
+
 
 def test_update():
     conn, cursor = connectDB(DATABASE_NAME)
     createDB(cursor)
-    insertDB(cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500)
-    cursor.execute("SELECT * FROM jobs WHERE job_id=?", (55176,))
+    insertDB(
+        cursor, 55177, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500  # 注意这里，job_id 改为 55177
+    )
+    updateDB(
+        cursor, 55177, "2022-10-01"
+    )
+    cursor.execute("SELECT * FROM jobs WHERE job_id=?", (55177,))
     result = cursor.fetchone()
-    assert result == (55176, "Amazon", "Fullstack Developer", "2022-09-30", "New Grads", 550)
+    assert result == (
+        55177,
+        "Amazon",
+        "Software Engineer",
+        "2022-10-01",
+        "New Grads",
+        500,
+    )
+
+
 
 def test_delete():
     conn, cursor = connectDB(DATABASE_NAME)
     createDB(cursor)
-    insertDB(cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500)
-    insertDB(cursor, 87356, "Google", "Backend Developer", "2022-09-20", "Experienced", 300)
-    
+    insertDB(
+        cursor, 55176, "Amazon", "Software Engineer", "2022-09-30", "New Grads", 500
+    )
+    insertDB(
+        cursor, 87356, "Google", "Backend Developer", "2022-09-20", "Experienced", 300
+    )
+
     deleteDB(cursor, 55176)
     info = readDB(cursor)
     assert len(info) == 1
+
 
 def test_query1():
     if os.path.exists("jobsDB.db"):
         os.remove("jobsDB.db")
     conn, cursor = connectDB(DATABASE_NAME)
     # 使用insertDB来插入测试数据
-    insertDB(cursor, 55176, "Amazon", "Fullstack Developer", "2022-09-30", "New Grads", 550)
+    insertDB(
+        cursor, 55176, "Amazon", "Fullstack Developer", "2022-09-30", "New Grads", 550
+    )
     result = query1(cursor)
     assert result == 1  # 我们刚刚插入了一个工作岗位
+
 
 def test_query2():
     if os.path.exists("jobsDB.db"):
         os.remove("jobsDB.db")
     conn, cursor = connectDB(DATABASE_NAME)
-    insertDB(cursor, 55176, "Amazon", "Fullstack Developer", "2021-09-29", "New Grads", 550)
+    insertDB(
+        cursor, 55176, "Amazon", "Fullstack Developer", "2021-09-29", "New Grads", 550
+    )
     results = query2(cursor)
     for job in results:
-        assert job[3] < '2022-01-01'  # 确保工作发布日期在2022年1月1日之前
+        assert job[3] < "2022-01-01"  # 确保工作发布日期在2022年1月1日之前
